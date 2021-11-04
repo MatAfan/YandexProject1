4f27774130e36cd9f06bb714bdff795b64c13c35
Афанасьев <matvey.afanasev986@gmail.com>
2021-11-04 10:42:14 +0300

    Описание и тз

--- MainMenu.py
+++ MainMenu.py
@@ -11,21 +11,23 @@ class MainMenu(QMainWindow):
         self.btn_add.clicked.connect(self.add_menu)
         self.btn_change.clicked.connect(self.change_menu)
         self.btn_get.clicked.connect(self.get_menu)
-        self.btn_changeother.clicked.connect(self.change_other_menu)
 
     def add_menu(self):
-        ex = AddOtherMenu
-        ex.show()
+        pass
 
     def change_menu(self):
-        ex = ChangeMenu
+        app = QApplication(sys.argv)
+        ex = ChangeMenu()
         ex.show()
 
     def get_menu(self):
         pass
 
     def change_other_menu(self):
-        pass
+        app = QApplication(sys.argv)
+        ex = AddOtherMenu()
+        ex.show()
+
 
 
 if __name__ == '__main__':
