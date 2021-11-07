9df6d1a1bd36f006a83e212660c67320af2fde08
Афанасьев <matvey.afanasev986@gmail.com>
2021-11-07 14:56:09 +0300

    Finally... Did it... After so many days...

--- imports.py
+++
@@ -1,9 +0,0 @@
-import sys
-import datetime as dt
-from docxtpl import DocxTemplate
-import sqlite3
-from PyQt5 import uic
-from PyQt5.QtWidgets import *
-from PyQt5.QtWidgets import QMainWindow
-
-DBNAME = 'project_db.db'
